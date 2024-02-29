05d5826d517b36c6983bb0dd018c24fafb3c2868
Amaro Suárez <amaro.suarez@iesnervion.es>
2024-02-29 13:19:25 +0100

    Comments in English

--- src/project/Dashboard.java
+++ src/project/Dashboard.java
@@ -3,25 +3,28 @@ package project;
 public class Dashboard {
 
 	/**
-	 * Array que almacena el tablero de barcos del jugador
+	 * Array storing the player's ship board
 	 */
 	char[][] TABLERO_BARCOS_JUGADOR = new char[5][5];
 	
 	/**
-	 * Array que almacena el tablero de disparos del jugador
+	 * Array that stores the player's shot board
 	 */
 	char[][] TABLERO_DISPAROS_JUGADOR = new char[5][5];
 	
 	/**
-	 * Array que almacena el tablero de barcos de la máquina
+	 * Array storing machine ship board
 	 */
 	char[][] TABLERO_BARCOS_MAQUINA = new char[5][5];
 	
 	/**
-	 * Array que almacena el tablero de disparos de la máquina
+	 * Array that stores the machine's trigger board
 	 */
 	char[][] TABLERO_DISPAROS_MAQUINA = new char[5][5];
 	
 	
+	void setBoats() {
+		
+	}
 	
 }
