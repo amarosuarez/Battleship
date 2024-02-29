e2e8ac6ba46abf0cb5f11d5ae2dd5bd6bf328dea
Amaro Suárez <amaro.suarez@iesnervion.es>
2024-02-29 13:13:41 +0100

    Arrays for dashboards

--- src/project/Dashboard.java
+++ src/project/Dashboard.java
@@ -2,4 +2,26 @@ package project;
 
 public class Dashboard {
 
+	/**
+	 * Array que almacena el tablero de barcos del jugador
+	 */
+	char[][] TABLERO_BARCOS_JUGADOR = new char[5][5];
+	
+	/**
+	 * Array que almacena el tablero de disparos del jugador
+	 */
+	char[][] TABLERO_DISPAROS_JUGADOR = new char[5][5];
+	
+	/**
+	 * Array que almacena el tablero de barcos de la máquina
+	 */
+	char[][] TABLERO_BARCOS_MAQUINA = new char[5][5];
+	
+	/**
+	 * Array que almacena el tablero de disparos de la máquina
+	 */
+	char[][] TABLERO_DISPAROS_MAQUINA = new char[5][5];
+	
+	
+	
 }
