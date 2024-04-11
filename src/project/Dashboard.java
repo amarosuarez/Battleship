906cecbbc162d3bdbe98e12830aaf7a30cdd1dfd
jmanuel-mg98 <148326871+jmanuel-mg98@users.noreply.github.com>
2024-04-11 13:19:58 +0200

    subida tablero

--- src/project/Dashboard.java
+++ src/project/Dashboard.java
@@ -2,4 +2,21 @@ package project;
 
 public class Dashboard {
 
+	public static void dashboardInit(char[][] dashboard) {
+		for (int i = 0; i < dashboard.length; i++) {
+			for (int j = 0; j < dashboard[i].length; j++) {
+				dashboard[i][j] = '*';
+			}
+		}
+	}
+
+	public static void seeDashboard(char[][] dashboard) {
+		for (int i = 0; i < dashboard.length; i++) {
+			for (int j = 0; j < dashboard[i].length; j++) {
+				System.out.print(dashboard[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
 }
